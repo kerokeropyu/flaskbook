e5781b6dc349052d62286c9341d8f8ed08741452
奥　尚人 <okunaoto@mbp.local>
2023-08-12 22:40:21 +0900

    お問合せフォームの作成

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import (
+    Flask, 
+    render_template, 
+    url_for, 
+    current_app, 
+    g, 
+    request, 
+    redirect, 
+    flash
+)
+from email_validator import validate_email, EmailNotValidError
 
+# Flaskクラスをインスタンス化する
 app = Flask(__name__)
-
-@app.route("/", endpoint="endpoint-name")
+# SECRET_KEYを追加する
+app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
+@app.route("/", endpoint="index")
 def index():
     return "Hello, Flaskbook!"
 
@@ -13,3 +25,76 @@ def index():
            endpoint="hello-endpoint")
 def hello(name):
     return f"Hello, {name}!"
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
+
+# with app.test_request_context():
+#     # /
+#     print(url_for("index"))
+#     # /hello/world
+#     print(url_for("hello-endpoint", name="world"))
+#     # /name/ichiro?page=1
+#     print(url_for("show_name"), name="ichiro", page="1")
+
+# アプリケーションコンテキストを取得してスタックへpushする
+ctx = app.app_context()
+ctx.push()
+
+# current_appにアクセスが可能になる
+print(current_app.name)
+
+# グローバルなテンポラリ領域に値を設定する
+# データベースの接続接続などに利用する
+g.connection = "connection"
+print(g.connection)
+
+# リクエストコンテキスト
+with app.test_request_context("/users?updated=true"):
+    # trueが出力される
+    print(request.args.get("updated"))
+
+# お問い合わせページを表示する
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+# お問い合わせ完了
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        # form属性を使ってフォームの値を取得する
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        # 入力チェック
+        is_valid = True
+
+        if not username:
+            flash("ユーザー名は必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid = False
+
+        if not description:
+            flash("お問い合わせ内容は必須です。")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+        # メールを送る
+
+        flash("お問い合わせ内容はメールにて送信しました。お問合せありがとうございます。")
+        # contactエンドポイントへリダイレクトする
+        return redirect(url_for("contact_complete"))
+    return render_template("contact_complete.html")
